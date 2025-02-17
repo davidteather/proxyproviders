8831f701cb0972052e469eb3ef8f499c0a4f0247
davidteather <34144122+davidteather@users.noreply.github.com>
2025-02-17 16:24:20 -0600

    fix: docs

--- .sphinx/conf.py
+++ .sphinx/conf.py
@@ -47,8 +47,6 @@ napoleon_google_docstring = True
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ['_static']
 html_baseurl = "https://davidteather.github.io/proxyproviders/"
 
-source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 source_suffix = {".rst": "restructuredtext", ".md": "markdown", ".txt": "rst"}
